70ba44b000d9e9958bc9a32b40fe991e8f4f6edf
paulo.fernandes <paulo.fernandes@payshopx.com.br>
2020-10-29 07:28:44 -0300

    primeiras implementações do projeto

--- src/main/java/com/hibicode/beerstore/resource/BeerResource.java
+++ src/main/java/com/hibicode/beerstore/resource/BeerResource.java
@@ -1,18 +1,39 @@
 package com.hibicode.beerstore.resource;
 
-import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hibicode.beerstore.model.Beer;
+import com.hibicode.beerstore.repository.Beers;
+
+//@Validated
 @RestController
 @RequestMapping("/beers")
 public class BeerResource {
 
+	@Autowired
+	private Beers beers;
+	
 	@GetMapping
-	public List<String> all() {
-		return Arrays.asList("Heineken", "Colorado Indiga", "Stella Artois");
+	public List<Beer> all() {
+		//return Arrays.asList("Heineken", "Colorado Indiga", "Stella Artois");
+		return beers.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Beer create(@Valid @RequestBody Beer beer) {
+		return beers.save(beer);
 	}
 }
